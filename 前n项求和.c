e76fb95514b9c8fd246f1007b5307cd2f9c93317
“SmithDavid” <zhiyuan_dong2006@qq.com>
2024-11-10 20:51:00 +0800

    update

--- 前n项求和.c
+++ 前n项求和.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/*#include <stdio.h>
 
 int main() {
 int n, i;
@@ -6,6 +6,22 @@ double sum = 0.0;
     scanf("%d", &n);
     for (i = 1; i <= n; i++) {
         sum =sum + 1.0/i;
+    }
+    printf("f(%d) = %f", n,sum);
+    return 0;
+}*/
+
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    double sum = 0.0;
+    scanf("%d", &n);
+    int fu = -1;                //int fu = 1; 
+                              // for循环里就加上fu = -fu;
+    for (i = 1; i <= n; i++) {                  
+        sum =sum + fu*1.0/i;//第一项符号为负
+       
     }
     printf("f(%d) = %f", n,sum);
     return 0;
