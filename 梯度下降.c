abbce990f3c39a1f1adeea8dab01c56330b7fc96
“SmithDavid” <zhiyuan_dong2006@qq.com>
2025-01-05 14:19:00 +0800

    end

--- 梯度下降.c
+++ 梯度下降.c
@@ -2,24 +2,15 @@
 int main() {
     double x[100];
     double y[100];
-    // printf("Enter the number of data points: ");
+    printf("Enter the number of data points: ");
     int num = 0;
-    // scanf("%d", &num);
-    // int i = 0;
-    // while(i < num){
-    // scanf("%lf,%lf", &x[i], &y[i]);
-    // i++;
-    // }
-    num = 8;
-    x[0] = -10;y[0] = 55;
-    
-    x[1] = 2;y[1] = -30;
-    x[2] = 3; y[2] = 77;
-    x[3] = 4;y[3] = -44;
-    x[4] = 23; y[4] =-99;
-    x[5] = 6; y[5] = -57;
-    x[6] = 7; y[6] = -113;
-    x[7] = -22; y[7] = 150;
+    scanf("%d", &num);
+    int i = 0;
+    while(i < num){
+    scanf("%lf,%lf", &x[i], &y[i]);
+    i++;
+    }
+   
     double k = 0;
     double b = 0;
    double learning_rate = 0.01;
